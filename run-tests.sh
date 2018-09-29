63dd985060a90f672a48a56e54276d2da0f65349
Yi Yang <Yi_Yang@StateStreet.com>
2018-09-28 22:46:16 -0400

    Added aws cli scripts

--- run-tests.sh
+++ run-tests.sh
@@ -6,4 +6,7 @@ source s3-deployment-info/deployment-info.conf
 export SAMPLE_APP_URL
 env
 
-
+git clone git-selenium-tests selenium-output-dir
+cd selenium-output-dir
+mvn compile test-compile surefire:test
+ls screenshots
