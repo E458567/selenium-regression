8d3166bda58ba95fd75cd8f7f6c9a4c996af5228
Yi Yang <Yi_Yang@StateStreet.com>
2018-10-02 13:03:12 -0400

    Updated folder names

--- run-tests.sh
+++ run-tests.sh
@@ -2,11 +2,11 @@
 
 set -e -x
 pwd
-source s3-deployment-info/deployment-info.conf
+source s3-deployment-output/deployment-info.conf
 export SAMPLE_APP_URL
 env
 
-git clone git-selenium-tests selenium-output-dir
+git clone git-qa-regression selenium-output-dir
 cd selenium-output-dir
 mvn compile test-compile surefire:test
 ls screenshots
